677081efbf05bff91683c43b6c80f8c1e26c4d4e
Lucas <lucaspmoraes603@gmail.com>
2026-09-08 14:07:28 -0300

    Primeiro commit

---
+++ app/clientes/page.tsx
@@ -0,0 +1,7 @@
+export default function Clientes() {
+  return (
+    <header className="w-full border-b text-black border-gray-300 h-16 px-10 justify-start content-center">
+      Clientes
+    </header>
+  );
+}
